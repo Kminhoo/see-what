cd9032a43460677df15db96cda50ff8a6d56d029
jinsil-kim <wlstlf9315@gmail.com>
2024-12-26 20:42:27 +0900

    style: max-width 1280px 설정

--- src/app/theater-list/page.tsx
+++ src/app/theater-list/page.tsx
@@ -11,17 +11,17 @@ const TheaterListPage = () => {
   const filteredTheater = MOCK_DATA.filter((theater) => theater.name?.includes(searchTheater));
 
   return (
-    <>
-        <div className="mt-20 top-0 z-10 p-4 flex justify-center gap-3 items-center">
-          <SearchIcon />
-          <input
-            type="text"
-            placeholder="검색어를 입력하세요."
-            value={searchTheater}
-            onChange={(e) => setSearchTheater(e.target.value)}
-            className="p-1 border-b-2 w-[500px] border-white bg-transparent text-white focus:outline-none focus:border-darkGray"
-          />
-        </div>
+    <div className="max-w-screen-xl mx-auto">
+      <div className="mt-20 top-0 z-10 p-4 flex justify-center gap-3 items-center ">
+        <SearchIcon />
+        <input
+          type="text"
+          placeholder="검색어를 입력하세요."
+          value={searchTheater}
+          onChange={(e) => setSearchTheater(e.target.value)}
+          className="p-1 border-b-2 w-[500px] border-white bg-transparent text-white focus:outline-none focus:border-darkGray"
+        />
+      </div>
 
       <div>
         {filteredTheater.length > 0 ? (
@@ -34,7 +34,7 @@ const TheaterListPage = () => {
           <p className="text-white text-center h-[calc(100vh-20rem)] mt-20">검색 결과가 없습니다.</p>
         )}
       </div>
-    </>
+    </div>
   );
 };
 
