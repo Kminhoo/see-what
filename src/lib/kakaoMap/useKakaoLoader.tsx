493aff4ecbf226c6fafadadffe48a1488aa1fd6a
jinsil-kim <wlstlf9315@gmail.com>
2024-12-27 20:27:27 +0900

    fix: 에러 추가

--- src/lib/kakaoMap/useKakaoLoader.tsx
+++ src/lib/kakaoMap/useKakaoLoader.tsx
@@ -2,6 +2,10 @@ import { useKakaoLoader as useKakaoLoaderOrigin } from "react-kakao-maps-sdk"
 
 export default function useKakaoLoader() {
     const appkey = process.env.NEXT_PUBLIC_KAKAOMAP_KEY;
+
+    if(!appkey) {
+      throw new Error("KaKao API Key is missing.")
+    }
     
   useKakaoLoaderOrigin({
     appkey,
