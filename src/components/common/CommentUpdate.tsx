c042b38d435a00267aaeb30aecb592d812a58293
kimmunsik20240905 <metalskin82@gmail.com>
2024-12-29 16:23:55 +0900

    refactor: 풀을 위한 임시 커밋

--- src/components/common/CommentUpdate.tsx
+++ src/components/common/CommentUpdate.tsx
@@ -15,11 +15,9 @@ const CommentUpdate = ({
   onCancel
 }: CommentUpdateProps): JSX.Element => {
   const [newComment, setNewComment] = useState<string>(initialValue);
-  const [isUpdating, setIsUpdating] = useState<boolean>(false);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setIsUpdating(true);
 
     try {
       const updatedComment = await updateComment(tableName, {
@@ -32,8 +30,6 @@ const CommentUpdate = ({
     } catch (error) {
       alert('댓글 수정 중 오류가 발생했습니다.');
       console.error(error);
-    } finally {
-      setIsUpdating(false);
     }
   };
 
@@ -43,7 +39,7 @@ const CommentUpdate = ({
         value={newComment}
         onChange={(e) => setNewComment(e.target.value)}
         onSubmit={handleSubmit}
-        buttonText={isUpdating ? '수정 중...' : '수정'}
+        buttonText={'수정'}
       />
       <Button type="button" className="text-sm text-gray-500 mt-2" onClick={onCancel}>
         취소
